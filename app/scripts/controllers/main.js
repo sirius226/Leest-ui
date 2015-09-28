be0c441be015332734171f853fd8b38d03a204c1
cli <cli@qmedichealth.com>
2015-09-28 18:24:40 -0400

    add css and index files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the leestUiApp
  */
 angular.module('leestUiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$log', function ($scope, $log) {
+    var vm = this;
+    vm.todos = [{title:'Angular', completed: false}, {title:'HTML', completed:true}, {title: 'CSS', completed:true}, {title:'Javascript', completed: false}]
+    vm.remainingCount = 1;
+    vm.newTodo = {title: '', completed: false};
+    vm.allChecked = true;
+    vm.editedTodo = {};
+
+    vm.addTodo = function(){
+      vm.todos.push(vm.newTodo);
+      $log.debug(vm.todos);
+      vm.newTodo = {title: '', completed: false};
+    }
+
+    vm.markAll = function(){
+      for(var i in vm.todos){
+        vm.todos[i].completed = vm.allChecked;
+      }
+      $log.debug(vm.allChecked);
+    }
+
+    vm.editTodo = function(todo){
+      vm.editedTodo = todo;
+
+    }
+
+  }]);
